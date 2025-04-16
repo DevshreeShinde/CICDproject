32acdedf58f23ea1c512b3f81dc219c6747cb38d
devshreeshinde <deveshreeshinde@gmail.com>
2025-04-16 18:15:40 +0530

    New CICD project

---
+++ src/main/java/org/example/cicdproject/CicDprojectApplication.java
@@ -0,0 +1,13 @@
+package org.example.cicdproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicDprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicDprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cicdproject/CicDprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cicdproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicDprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
